a628936003531f3d1dfb9cc124d5fc95b4dd0161
otto hoffman <otto.hoffman@outlook.com>
2024-03-20 10:58:09 +0100

    uppg1_feldebugging

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int[] array = {3,2,5,4,6,8,9,7};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(i+" har värdet "+array[i]);
+        }
+        boolean fort = true;
+        while(fort==true){
+            System.out.println("En gång till");
+        }
+    }
+}
