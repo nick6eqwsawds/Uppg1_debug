55f57b15c3df778712e980fd5890e4a694cf3345
otto hoffman <otto.hoffman@outlook.com>
2024-03-20 11:10:30 +0100

    del c

--- src/App.java
+++ src/App.java
@@ -1,6 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
+        int slump = (int) Math.random()*100;
+        slump-=49;
+        if(slump < 0 ){
+            System.out.println("Slumptalet är negativt");
+        }
+        else {
+            System.out.println("slumptalet är positivt");
+        }
         int[] array = {3,2,5,4,6,8,9,7};
 
         for (int i = 5; i < array.length; i++) {
